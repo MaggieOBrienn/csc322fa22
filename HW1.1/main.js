c47e0b69f7ac2f506a2c540965e9101e06b76d32
MaggieOBrienn <maggob12@gmail.com>
2023-09-12 11:48:17 -0400

    HW1.1Update

--- HW1.1/main.js
+++ HW1.1/main.js
@@ -10,7 +10,6 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//face 1
 const points = [];
 points.push(new THREE.Vector3(-10,0,0));
 points.push(new THREE.Vector3(0,15,0));
@@ -21,30 +20,5 @@ const face1 = new THREE.BufferGeometry().setFromPoints(points);
 const material1 = new THREE.LineBasicMaterial({color: 0xffffff});
 const line = new THREE.Line(face1,material1);
 
-//face 2
-/*const points2 =[];
-points2.push(new THREE.Vector3(-10,0,0));
-points2.push(new THREE.Vector3(0,-3,0));
-points2.push(new THREE.Vector3(-2,-10,0));
-points2.push(new THREE.Vector3(-10,0,0));
-
-const face2 = new THREE.BufferGeometry().setFromPoints(points2);
-const material2 = new THREE.MeshBasicMaterial({color: 0x4682B4});
-const mesh2 = new THREE.Line(face2,material2);
-
-//face 3
-const points3 = [];
-points3.push(new THREE.Vector3(0,-3,0));
-points3.push(new THREE.Vector3(5,4,0));
-points3.push(new THREE.Vector3(-2,-10,0));
-points3.push(new THREE.Vector3(0,-3,0));
-
-
-const face3 = new THREE.BufferGeometry().setFromPoints(points3);
-const material3 = new THREE.MeshBasicMaterial({color: 0x87CEFA});
-const mesh3 = new THREE.Line(face3,material3);*/
-
 scene.add(line);
-//scene.add(mesh2);
-//scene.add(mesh3);
 renderer.render(scene,camera);
